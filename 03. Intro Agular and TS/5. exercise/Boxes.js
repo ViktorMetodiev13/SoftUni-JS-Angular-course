f9af0ab4458cd610d15db9fb65463879539ac6ac
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-12-03 18:56:31 +0200

    Exercise 5 fully complete

---
+++ 03. Intro Agular and TS/5. exercise/Boxes.js
@@ -0,0 +1,26 @@
+var Box = /** @class */ (function () {
+    function Box() {
+        this._boxes = [];
+    }
+    Box.prototype.add = function (el) {
+        this._boxes.push(el);
+    };
+    Box.prototype.remove = function () {
+        this._boxes.pop();
+    };
+    Object.defineProperty(Box.prototype, "count", {
+        get: function () {
+            return this._boxes.length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Box;
+}());
+var box = new Box();
+box.add(1);
+box.add(2);
+box.add(3);
+console.log(box.count);
+box.remove();
+console.log(box.count);
--- 03. Intro Agular and TS/5. exercise/Boxes.ts
+++ 03. Intro Agular and TS/5. exercise/Boxes.ts
@@ -14,8 +14,9 @@ class Box<T> {
     }
 } 
 
-let box = new Box<Number>();
-box.add(1);
-box.add(2);
-box.add(3);
+let box = new Box<String>();
+box.add("Pesho");
+box.add("Gosho");
+console.log(box.count);
+box.remove(); 
 console.log(box.count);
