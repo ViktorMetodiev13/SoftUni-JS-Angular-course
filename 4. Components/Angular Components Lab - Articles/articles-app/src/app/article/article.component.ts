ac8706d67958470e09bc3c494e176356955d5d4a
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-12-11 16:39:14 +0200

    Show more functionality added and hide more

--- 4. Components/Angular Components Lab - Articles/articles-app/src/app/article/article.component.ts
+++ 4. Components/Angular Components Lab - Articles/articles-app/src/app/article/article.component.ts
@@ -8,8 +8,8 @@ import { Article } from '../models/article.model';
 })
 export class ArticleComponent {
   private symbols: number = 250;
-  @Input() article: Article;
-  @Input() articleDesc: string;
+  @Input() article!: Article;
+  @Input() articleDesc!: string;
   descToShow: string;
   articleDescLen: number;
   showReadMoreBtn: boolean = true;
@@ -20,5 +20,27 @@ export class ArticleComponent {
   constructor() {
     this.articleDescLen = 0;
     this.descToShow = "";
+  }
+
+  readMore(): void {
+    this.articleDescLen += this.symbols;
+    if (this.articleDescLen >= this.articleDesc.length) {
+      this.showHideBtn = true;
+      this.showReadMoreBtn = false;
+    } else {
+      this.descToShow = this.articleDesc.substring(0, this.articleDescLen);
+    }
+  }
+
+  hideDesc(): void {
+    this.descToShow = "";
+    this.articleDescLen = 0;
+    this.showHideBtn = false;
+    this.showReadMoreBtn = true;
+  }
+
+  toggleImage(): void {
+    this.imageIsShown = !this.imageIsShown;
+    this.imageButtonTitle = this.imageButtonTitle === "Show Image" ? "Hide Image" : "Show Image";
   }
 }
