5ac2d04935613b0fe2662ab10a5c04384e409f65
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-12-18 16:14:53 +0200

    created dummy user to test login and logout

--- Course WorkShop/Workshop solution/app/src/app/auth/login/login.component.ts
+++ Course WorkShop/Workshop solution/app/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private router: Router, private authService: AuthService) {
+    this.authService.user = {
+      username: 'John'
+    }
+
+    this.router.navigate(['/'])
+  }
 }
